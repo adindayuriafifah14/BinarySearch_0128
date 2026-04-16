1bf6a680221746988274805d3fa4ade48f1e6195
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-16 11:11:59 +0700

    Menambahkan binary search dengan kondisi if

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -91,7 +91,17 @@ void binarySearch()
                 cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
                 return;
             }
-        } 
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x > element [mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        if (low > high)
         
     
     }
