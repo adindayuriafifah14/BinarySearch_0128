8474c31a89a4a8246f5d66384f53362f8f68dc6e
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-04-16 11:03:10 +0700

    Menambahkan fungsi binary search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -79,7 +79,20 @@ void binarySearch()
 
         cout << "Masukkan elemen yang ingin dicari: ";
         cin >> x;
-        int
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+
+            if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+        } 
+        
     
     }
     
